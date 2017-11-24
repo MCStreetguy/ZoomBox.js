730f0b011be37c1093e7793d97f03be240933345
Max Schmidt <hackingmcs@gmail.com>
2017-11-24 16:38:04 +0100

    WIP: Main functionality

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const pump = require('pump');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
 
 function buildJQuery() {
   pump([
@@ -15,7 +16,11 @@ function buildJQuery() {
         this.emit('end');
       }
     }),
-    gulp.src('src/jquery.js'),
+    gulp.src([
+      'bower_components/slick-carousel/slick/slick.js',
+      'src/jquery.js'
+    ]),
+    concat('main.js'),
     uglify(),
     rename('zoombox.jquery.min.js'),
     gulp.dest('dist/')
@@ -24,18 +29,18 @@ function buildJQuery() {
 gulp.task('build-jQuery',buildJQuery);
 
 function buildPlain() {
-  pump([
-    plumber({
-      errorHandler: function(err) {
-        console.log(err);
-        this.emit('end');
-      }
-    }),
-    gulp.src('src/plain.js'),
-    uglify(),
-    rename('zoombox.min.js'),
-    gulp.dest('dist/')
-  ])
+  // pump([
+  //   plumber({
+  //     errorHandler: function(err) {
+  //       console.log(err);
+  //       this.emit('end');
+  //     }
+  //   }),
+  //   gulp.src('src/plain.js'),
+  //   uglify(),
+  //   rename('zoombox.min.js'),
+  //   gulp.dest('dist/')
+  // ])
 }
 gulp.task('build-plain',buildPlain);
 
@@ -47,7 +52,11 @@ function buildCss() {
         this.emit('end');
       }
     }),
-    gulp.src('src/styles.scss'),
+    gulp.src([
+      'bower_components/slick-carousel/slick/slick.sass',
+      'src/styles.scss'
+    ]),
+    concat('main.scss'),
     sass(),
     cleanCSS(),
     autoprefixer({
--- src/jquery.js
+++ src/jquery.js
@@ -17,7 +17,7 @@
     theme: 'dark'
   }
 
-  var createOverlay = function (options) {
+  function createOverlay(options) {
     if($('#'+options.containerId).length) destroyOverlay(options);
 
     $(document.body).css('overflow','hidden');
@@ -32,7 +32,8 @@
 
     if(options.closeOnBackClick) {
       elem.on('click',function (event) {
-        if(!$(event.target).is('img')) destroyOverlay(options);
+        console.log(event);
+        if(!$(event.target).is('img, .slick-arrow, .slick-arrow *')) destroyOverlay(options);
       })
     } else {
       elem.on('click',function (event) {
@@ -47,7 +48,7 @@
     return elem;
   }
 
-  var destroyOverlay = function (options) {
+  function destroyOverlay(options) {
     if($('#'+options.containerId).length) {
       $(document.body).css('overflow','').find('#'+options.containerId).removeClass('in').one('transitionend',function () {
         $(this).remove();
@@ -56,6 +57,12 @@
     } else {
       return false;
     }
+  }
+
+  function centerVertical(parent,selector) {
+    $(selector).each(function (i,e,a) {
+      $(this).css('margin-top',($(parent).innerHeight() - $(this).height()) / 2);
+    })
   }
 
   $.fn.zoombox = function(options) {
@@ -70,16 +77,42 @@
     }
 
     this.each(function (index,element,array) {
-      var src = (options.useDataSource ? $(element).data('src') : $(element).attr('src'));
+      var src = [(options.useDataSource ? $(element).data('src') : $(element).attr('src'))];
+      $($(element).data('parent')).children().not(element).each(function (i,e,a) {
+        src.push((options.useDataSource ? $(e).data('src') : $(e).attr('src')));
+      })
 
       $(element).on('click',function (event) {
         var over = createOverlay(options);
-        over.find('.'+options.innerClass).append('<img src="' + src + '" class="' + options.imageClass + '" />');
+
+        src.forEach(function (e,i,a) {
+          over.find('.'+options.innerClass).append('<img src="' + e + '" class="' + options.imageClass + '" />');
+        })
+
+        if(src.length > 1) {
+          $('#'+options.containerId).find('.'+options.innerClass).slick({
+            prevArrow: '<button type="button" class="slick-prev"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 8 14"><path d="M4.898 10.75q0 0.102-0.078 0.18l-0.391 0.391q-0.078 0.078-0.18 0.078t-0.18-0.078l-3.641-3.641q-0.078-0.078-0.078-0.18t0.078-0.18l3.641-3.641q0.078-0.078 0.18-0.078t0.18 0.078l0.391 0.391q0.078 0.078 0.078 0.18t-0.078 0.18l-3.070 3.070 3.070 3.070q0.078 0.078 0.078 0.18zM7.898 10.75q0 0.102-0.078 0.18l-0.391 0.391q-0.078 0.078-0.18 0.078t-0.18-0.078l-3.641-3.641q-0.078-0.078-0.078-0.18t0.078-0.18l3.641-3.641q0.078-0.078 0.18-0.078t0.18 0.078l0.391 0.391q0.078 0.078 0.078 0.18t-0.078 0.18l-3.070 3.070 3.070 3.070q0.078 0.078 0.078 0.18z"></path></svg></button>',
+            nextArrow: '<button type="button" class="slick-next"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 8 14"><path d="M4.648 7.5q0 0.102-0.078 0.18l-3.641 3.641q-0.078 0.078-0.18 0.078t-0.18-0.078l-0.391-0.391q-0.078-0.078-0.078-0.18t0.078-0.18l3.070-3.070-3.070-3.070q-0.078-0.078-0.078-0.18t0.078-0.18l0.391-0.391q0.078-0.078 0.18-0.078t0.18 0.078l3.641 3.641q0.078 0.078 0.078 0.18zM7.648 7.5q0 0.102-0.078 0.18l-3.641 3.641q-0.078 0.078-0.18 0.078t-0.18-0.078l-0.391-0.391q-0.078-0.078-0.078-0.18t0.078-0.18l3.070-3.070-3.070-3.070q-0.078-0.078-0.078-0.18t0.078-0.18l0.391-0.391q0.078-0.078 0.18-0.078t0.18 0.078l3.641 3.641q0.078 0.078 0.078 0.18z"></path></svg></button>'
+          });
+
+          centerVertical(over.find('.'+options.innerClass),'.slick-slide');
+
+          $(window).on('resize',function () {
+            centerVertical(over.find('.'+options.innerClass),'.slick-slide');
+          });
+        } else {
+          centerVertical(over.find('.'+options.innerClass),over.find('.'+options.imageClass));
+
+          $(window).on('resize',function () {
+            centerVertical(over.find('.'+options.innerClass),over.find('.'+options.imageClass));
+          })
+        }
+
         setTimeout(function () {
           over.addClass('in').one('transitionend',function () {});
         },50);
       })
-    }) 
+    })
 
     if(options.listenKeys) {
       $(document).on('keyup',function (event) {
