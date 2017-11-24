4ef2a777fe2a64eccb8a7e70d1ab09537e559a8c
Max Schmidt <hackingmcs@gmail.com>
2017-11-24 12:05:30 +0100

    WIP: Main Plugin functionality

--- src/jquery.js
+++ src/jquery.js
@@ -4,13 +4,32 @@
     autoInit: true
   }
 
+  var createOverlay = function () {
+    var tpl = '<div id="zoombox-overlay">';
+    tpl += '<div class="header">';
+    tpl += '<div class="close-btn">&times;</div>';
+    tpl += '</div>';
+    tpl += '<div class="body">';
+    tpl += '</div>';
+    tpl += '</div>';
+
+    $(document).append(tpl);
+  }
+
+  var destroyOverlay = function () {
+    $('#zoombox-overlay').remove();
+  }
+
   $.fn.zoombox = function(options) {
     if(options !== undefined && typeof options !== 'object') {
-      throw new TypeError('Invalid Arguments: options has to be an object.');
+      options = defaultValues;
     }
 
     this.each(function (i,e,a) {
       var src = this.attr('src');
+      this.on('click',function () {
+
+      })
     })
 
     return this;
